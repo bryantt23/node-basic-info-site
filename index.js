980e244d6082d36d63c283074dad60f477939e2d
bryant tunbutr <bryantt23@users.noreply.github.com>
2021-01-05 22:00:46 -0800

    Redo using express

--- index.js
+++ index.js
@@ -1,23 +1,22 @@
-const http = require('http');
-const fs = require('fs');
-
-// https://flaviocopes.com/node-serve-html-page/
-const server = http.createServer((req, res) => {
-  //   debugger;
-  const { url } = req;
-  if (url === '/index' || url === '/') {
-    res.writeHead(200, { 'content-type': 'text/html' });
-    fs.createReadStream('index.html').pipe(res);
-  } else if (url === '/about') {
-    res.writeHead(200, { 'content-type': 'text/html' });
-    fs.createReadStream('about.html').pipe(res);
-  } else if (url === '/contact-me') {
-    res.writeHead(200, { 'content-type': 'text/html' });
-    fs.createReadStream('contact-me.html').pipe(res);
-  } else {
-    res.writeHead(200, { 'content-type': 'text/html' });
-    fs.createReadStream('404.html').pipe(res);
-  }
+const express = require('express');
+const app = express();
+var path = require('path');
+// https://www.digitalocean.com/community/tutorials/use-expressjs-to-deliver-html-files
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname + '/index.html'));
 });
 
-server.listen(process.env.PORT || 8080);
+app.get('/about', function (req, res) {
+  res.sendFile(path.join(__dirname + '/about.html'));
+});
+
+app.get('/contact-me', function (req, res) {
+  res.sendFile(path.join(__dirname + '/contact-me.html'));
+});
+
+// https://stackoverflow.com/questions/11500204/how-can-i-get-express-js-to-404-only-on-missing-routes
+app.get('*', function (req, res) {
+  res.sendFile(path.join(__dirname + '/404.html'));
+});
+
+app.listen(8080);
